ca74cc1da74656088b52927b75c0446e30a1ebee
rockstardotb <alex37184@yahoo.com>
2018-08-12 17:50:30 -0500

    rearranged

---
+++ mLearning_2D/tests/test_preprocessing.py
@@ -0,0 +1,21 @@
+import pytest
+import preprocessing.preprocessing as prep
+import Data.csv
+
+# Test import
+X,y = prep.import_data(Data)
+
+# Test fix_missing
+X = prep.fix_missing(X)
+
+# Test categorical_encode
+X = prep.categorical_encode(X,True)
+y = prep.categorical_encode(y,False)
+
+# Test create_sets
+X_train,X_test,Y_train,Y_test = prep.create_sets(X,Y)
+
+# Test feature_scale
+X_train,X_test = prep.feature_scale(X_train,X_test)
+
+
