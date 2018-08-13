1f75b3b053dcd43a62e1a8112314963cd5f708bc
rockstardotb <alex37184@yahoo.com>
2018-08-13 17:14:33 -0500

    edited to get rid of hardcoded parameters

--- mLearning_2D/tests/test_preprocessing.py
+++ mLearning_2D/tests/test_preprocessing.py
@@ -4,17 +4,17 @@ import mLearning_2D.preprocessing.preprocessing as prep
 Data = './Data.csv'
 
 # Test import
-X,y = prep.import_data(Data)
+X,y = prep.import_data(Data,-1,3)
 
 # Test fix_missing
-X = prep.fix_missing(X)
+X = prep.fix_missing(X,1,3)
 
 # Test categorical_encode
-X = prep.categorical_encode(X,True,True)
-y = prep.categorical_encode(y,False)
+X = prep.categorical_encode(X,idx=0)
+y = prep.categorical_encode(y,independent=False)
 
 # Test create_sets
-X_train,X_test,Y_train,Y_test = prep.create_sets(X,y)
+X_train,X_test,Y_train,Y_test = prep.create_sets(X,y,size=0.2)
 
 # Test feature_scale
 X_train,X_test = prep.feature_scale(X_train,X_test)
